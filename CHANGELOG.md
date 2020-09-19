7dc3e42f85a73ee1fd03af2a5cd63870b6a39a56
Breno Costa <brenocosta0901@gmail.com>
2020-09-19 07:46:45 -0300

    Add first cookiecutter files

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## [0.1.0] - 2020-09-19 - @breno.costa
+
+### Added
+
+* Add first cookiecutter.json configuration file (author, author_email, project_name, project_directory, project_slug, project_description, version, command_line, license)
+* Add readme, changelog, dockerfile, makefile, setup.py, and requirements.txt files
+* Add default directories such as project_slug (templated) and tests
+* Create first version of the template based on the repo https://github.com/audreyr/cookiecutter-pypackage
---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='cookiecutter-python',
+    packages=[],
+    version='0.1.0',
+    description='Cookiecutter template for Python packages',
+    license='BSD',
+    keywords=['cookiecutter', 'template', 'package', ],
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
